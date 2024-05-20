1a550615caa11f7cd7b5e64536560449922f3afc
marym-ahmed <82773504+marym-ahmed@users.noreply.github.com>
2024-05-20 18:19:49 +0300

    first commit

---
+++ app/Http/Controllers/PictureController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Picture;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class PictureController extends Controller
+{
+
+public function store(Request $request)
+{
+    
+}
+
+}
